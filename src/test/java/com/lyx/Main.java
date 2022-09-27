ff57394d41d23ef680424b9e71d64ef0301d02de
李艳兴 <liyanxing@zhongruigroup.com>
2022-09-27 15:04:57 +0800

    完成

---
+++ src/test/java/com/lyx/Main.java
@@ -0,0 +1,17 @@
+package com.lyx;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.lang.Console;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        DateTime begin = DateUtil.parse("2020-04-18 17:00:00");
+
+        long days = DateUtil.betweenDay(DateUtil.date(), begin, true);
+
+        Console.log("打印数据：{}", days); // TODO 删除 李艳兴
+    }
+}
