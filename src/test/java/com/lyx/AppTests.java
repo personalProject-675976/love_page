254dbc53dd6c818edf1b8008fb532dacbc2e9932
李艳兴 <liyanxing@zhongruigroup.com>
2022-09-27 09:32:33 +0800

    添加音乐

---
+++ src/test/java/com/lyx/AppTests.java
@@ -0,0 +1,15 @@
+package com.lyx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
